b8520f1d8cb0a16524f9e579f3d9e0f2f0891288
Christian Vozar <christian@rogueethic.com>
2014-07-09 08:48:09 -0500

    Error strings should not end in punctuation

--- hipchat_output.go
+++ hipchat_output.go
@@ -115,11 +115,11 @@ func (ho *HipchatOutput) Init(config interface{}) (err error) {
 	ho.conf = config.(*HipchatOutputConfig)
 
 	if ho.conf.RoomID == "" {
-		return fmt.Errorf("room_id must contain a HipChat room ID or name.")
+		return fmt.Errorf("room_id must contain a HipChat room ID or name")
 	}
 
 	if len(ho.conf.From) > 15 {
-		return fmt.Errorf("from must be less than 15 characters.")
+		return fmt.Errorf("from must be less than 15 characters")
 	}
 
 	ho.url = "https://api.hipchat.com/v1"
